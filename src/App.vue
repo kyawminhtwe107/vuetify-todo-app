0179a1b544008d96121bb176eb29947bb490f578
Kyaw Min Htwe <kyawminhtwe107@gmail.com>
2021-08-13 16:49:10 +0630

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,86 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer 
+		v-model="drawer"
+		app
+	>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Vue Daily Task
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Best Todo Ever !
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+		  :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar> -->
+
+	<v-app-bar
+      app
+      color="#e62a00"
+      dark
+      shrink-on-scroll
+      src="https://cdn.pixabay.com/photo/2016/09/01/19/53/pocket-watch-1637396__340.jpg"
+	  prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Vue & Vuetify Daily Task</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+	
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    data: () => ({ 
+		items: [
+          { title: 'Todo', icon: 'mdi-format-list-checks', to:'/' },
+          { title: 'About', icon: 'mdi-help-box', to:'/about' },
+        ],
+		drawer: null 
+	}),
   }
-}
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,96 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  	<div class="home">
+
+		<v-text-field
+			class="pa-3"
+            outlined
+			hide-details
+            label="Add Task"
+            append-icon="mdi-plus"
+			clearable
+			v-model="newTask"
+			@click:append="addTask"
+			@keyup.enter="addTask"
+        ></v-text-field>
+		
+		<v-list
+			subheader
+			two-line
+			flat
+		>
+			<v-subheader>My Daily Tasks</v-subheader>
+
+			<v-list-item-group>
+				<v-list-item 
+					v-for="task in tasks" 
+					:key="task.id"
+					@click="doneTask(task.id)"
+					:class="{'blue lighten-5': task.done}"
+				>
+					<template v-slot:default>
+						<v-list-item-action>
+							<v-checkbox
+								:input-value="task.done"
+								color="primary"
+							></v-checkbox>
+						</v-list-item-action>
+
+						<v-list-item-content>
+							<v-list-item-title
+								:class="{'text-decoration-line-through': task.done}"
+							>
+								{{task.title}}
+							</v-list-item-title>
+						</v-list-item-content>
+						<v-list-item-action>
+							<v-btn 
+								icon
+								@click.stop="deleteTask(task.id)"
+							>
+								<v-icon color="red lighten-1">mdi-delete</v-icon>
+							</v-btn>
+						</v-list-item-action>
+					</template>
+				</v-list-item>
+
+			</v-list-item-group>
+		</v-list>
+		
+  	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  	export default {
+    	name: 'Home',
+
+		data(){
+			return{
+				newTask:'',
+				tasks:[
+					
+				]
+			}
+		},
+		methods:{
+			addTask(){
+				let task = {
+					id:this.tasks.length + 1,
+					title:this.newTask,
+					done:false
+				}
+
+				this.tasks.push(task)
+				this.newTask = ''
+			},
+			doneTask(id) {
+				let task = this.tasks.find(task => task.id === id)
+
+				task.done = !task.done
+			},
+			deleteTask(id){
+				this.tasks = this.tasks.filter(task => task.id !== id)
+			}
+		}
+  	}
 </script>
